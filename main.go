f717e46ccd7430fb71818de74e56663404b6981c
wch <wangchh@gmail.com>
2022-02-25 21:23:07 +0800

    optimized some codes

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"xxx/chain"
 	"xxx/config"
@@ -23,23 +24,6 @@ var mlog = log.New("main")
 func main() {
 	flag.Parse()
 
-	readConfig := func(path string, conf interface{}) {
-		_, err := toml.DecodeFile(path, conf)
-		if err != nil {
-			buf := new(bytes.Buffer)
-			encoder := toml.NewEncoder(buf)
-			err = encoder.Encode(conf)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(buf.String())
-			os.WriteFile(path, buf.Bytes(), 0666)
-		}
-	}
-	type logInfo struct {
-		path  string
-		level string
-	}
 	logCh := make(chan logInfo)
 	defer close(logCh)
 
@@ -52,49 +36,78 @@ func main() {
 
 	app := &cli.App{
 		Commands: []*cli.Command{
-			{
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:    "config",
-						Aliases: []string{"c"},
-						Usage:   "config file path",
-						Value:   "xxx.toml",
-					},
-				},
-				Name:    "consensus",
-				Aliases: []string{"cons"},
-				Usage:   "run consensus node",
-				Action: func(c *cli.Context) error {
-					conf := config.DefaultConsensusConfig
-					readConfig(c.String("config"), conf)
-					logCh <- logInfo{conf.LogPath, conf.LogLevel}
-					runConsensusNode(conf)
-					return nil
-				},
-			},
-			{
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:    "config",
-						Aliases: []string{"c"},
-						Usage:   "config file path",
-						Value:   "data.toml",
-					},
-				},
-				Name:    "data",
-				Aliases: []string{"dt"},
-				Usage:   "run data node",
-				Action: func(c *cli.Context) error {
-					conf := config.DefaultDataNodeConfig
-					readConfig(c.String("config"), conf)
-					logCh <- logInfo{conf.LogPath, conf.LogLevel}
-					runDataNode(conf)
-					return nil
-				},
-			},
+			consensusCmd(logCh),
+			datanodeCmd(logCh),
 		},
 	}
 	app.Run(os.Args)
+}
+
+type logInfo struct {
+	path  string
+	level string
+}
+
+func readConfig(path string, conf interface{}) {
+	_, err := toml.DecodeFile(path, conf)
+	if err != nil {
+		buf := new(bytes.Buffer)
+		encoder := toml.NewEncoder(buf)
+		err = encoder.Encode(conf)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(buf.String())
+		os.WriteFile(path, buf.Bytes(), 0666)
+	}
+}
+
+func datanodeCmd(logCh chan<- logInfo) *cli.Command {
+	return &cli.Command{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "config file path",
+				Value:   "data.toml",
+			},
+		},
+		Name:    "data",
+		Aliases: []string{"dt"},
+		Usage:   "run data node",
+		Action: func(c *cli.Context) error {
+			conf := config.DefaultDataNodeConfig
+			readConfig(c.String("config"), conf)
+			logCh <- logInfo{conf.LogPath, conf.LogLevel}
+			time.Sleep(time.Millisecond)
+			runDataNode(conf)
+			return nil
+		},
+	}
+}
+
+func consensusCmd(logCh chan<- logInfo) *cli.Command {
+	return &cli.Command{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "config file path",
+				Value:   "xxx.toml",
+			},
+		},
+		Name:    "consensus",
+		Aliases: []string{"cons"},
+		Usage:   "run consensus node",
+		Action: func(c *cli.Context) error {
+			conf := config.DefaultConsensusConfig
+			readConfig(c.String("config"), conf)
+			logCh <- logInfo{conf.LogPath, conf.LogLevel}
+			time.Sleep(time.Millisecond)
+			runConsensusNode(conf)
+			return nil
+		},
+	}
 }
 
 func runConsensusNode(conf *config.ConsensusConfig) {
